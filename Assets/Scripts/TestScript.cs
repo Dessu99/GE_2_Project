44f00e923a467cf64000b5240cd1e2aefe9ade4b
Dessu99 <dexterjameskirkup@yahoo.co.uk>
2025-01-27 11:00:28 +0200

    finding quick solutions
    ctrl + . for solutions

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace AG3959
@@ -7,6 +9,9 @@ namespace AG3959
 
         [SerializeField] Transform target;
         [SerializeField] float bustANumber = 5.0f;
+        [SerializeField] List<GameObject> listOfObjects;
+
+        // Changing the names of things as a whole ctrl + r and rename them all at the same time, easier for changing online scripts
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
