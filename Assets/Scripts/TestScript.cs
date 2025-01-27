0f7feb148434ccf802e9c57ca7e1613dd6f6a28b
Dessu99 <dexterjameskirkup@yahoo.co.uk>
2025-01-27 10:41:37 +0200

    TestScript addition

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace AG3959
+{
+    public class TestScript : MonoBehaviour
+    {
+
+        [SerializeField] Transform target;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
