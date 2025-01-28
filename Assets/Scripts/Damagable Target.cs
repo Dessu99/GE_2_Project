c1757c5f49c689d73c341521375e0ad1a7cf433c
Dessu99 <dexterjameskirkup@yahoo.co.uk>
2025-01-28 10:06:14 +0200

    a

---
+++ Assets/Scripts/Damagable Target.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DamagableTarget : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public void SwapColour()
+    {
+        if 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Shooting Target.cs
+++ Assets/Scripts/Shooting Target.cs
@@ -5,13 +5,13 @@ namespace Ag3959
     public class ShootingTarget : MonoBehaviour
     {
         [SerializeField] private Transform[] targetEndPoints;
-
-        public float speed = 1.0f;          // Adjust the speed for the application.
-
-
-        private Transform target;           // The target (cylinder) position.
-
         private int currentEndPointIndex = 0;
+        private Transform target;                               // The target (cylinder) position.
+        public float speed = 1.0f;                              // Adjust the speed for the application.
+
+
+
+
         void Start()
         {
             target = targetEndPoints[0];
@@ -19,25 +19,22 @@ namespace Ag3959
 
         void Update()
         {
-            // Move our position a step closer to the target.
 
             var step = speed * Time.deltaTime; // calculate distance to move
             transform.position = Vector3.MoveTowards(transform.position, target.position, step);
 
 
-            // Check if the position of the cube and sphere are approximately equal.
 
             if (Vector3.Distance(transform.position, target.position) < 0.001f)
             {
-                SwapEndPointTarget();                 // Swap the position of the cylinder.
-
+                SwapEndPointTarget();               
             }
 
             void SwapEndPointTarget()
 
             {
                 currentEndPointIndex++;
-                currentEndPointIndex = targetEndPoints.Length;
+                currentEndPointIndex %= targetEndPoints.Length;
                 target = targetEndPoints[currentEndPointIndex];
 
             }
